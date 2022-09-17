5c22c41b724cec5af5f2ac3d498ef4f20a3f8b84
Edison Acuna <edisonricardoac@hotmail.com>
2022-09-17 15:29:58 -0500

    added error message

--- AppointmentSystemUI/src/app/pages/registration/registration.component.ts
+++ AppointmentSystemUI/src/app/pages/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ApiUserService } from 'src/app/core/services/api/user/api.user.service';
 import { User } from 'src/app/shared/interfaces/user';
@@ -13,15 +14,17 @@ export class RegistrationComponent implements OnInit {
 
   private _subscriptions: Subscription = new Subscription();
   public registrationForm: FormGroup;
+  public errorMessage: string | undefined;
   constructor(
     private _formBuilder: FormBuilder,
     private _apiUserService: ApiUserService,
+    private _router: Router,
   ) {
     this.registrationForm = _formBuilder.group(
       {
-        firstName: new FormControl('', [Validators.required, Validators.minLength(3)]),
-        lastName: new FormControl('', [Validators.required, Validators.minLength(3)]),
-        id: new FormControl('', [Validators.required, Validators.minLength(3)]),
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        id: new FormControl('', [Validators.required, Validators.minLength(2)]),
       }
     );
   }
@@ -40,7 +43,10 @@ export class RegistrationComponent implements OnInit {
       this._apiUserService.createUser(user)
         .subscribe(
           (resp) => {
-            console.log('resp', resp);
+            this._router.navigate(['/booking'])
+          },
+          (error) => {
+            this.errorMessage = error;
           }
         )
     );
